0aa38f736b7c24155b07c05c82157454b9688141
alessanderleite <alessander.logistica@gmail.com>
2019-07-24 20:49:41 -0300

    initial commit

---
+++ src/main/java/br/com/alessanderleite/app/CreateExcelWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.alessanderleite.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateExcelWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateExcelWithSpringBootApplication.class, args);
+	}
+
+}
